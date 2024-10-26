6b44b50b03be9ca5efad1b1745ff41af745bb955
Hanz Bacus <bacushanzchester@gmail.com>
2024-10-26 14:02:09 +0800

    oct26

--- myproject/accounts/models.py
+++ myproject/accounts/models.py
@@ -13,7 +13,7 @@ class ProductCategory(models.Model):
 class Product(models.Model):
     product_name = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    product_price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.IntegerField()
     #category = models.ForeignKey(ProductCategory, on_delete=models.SET_NULL, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
@@ -66,3 +66,13 @@ class OrderItem(models.Model):
     
     def __str__(self):
         return f'{self.product.product_name} in Order {self.order.id}'
+    
+class payment_method:
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveBigIntegerField()
+    price = models.DecimalField(max_digits=0, decimal_places=2)
+    total = models.DecimalField(max_digits=0, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.id} in Order {self.order.id}' 
