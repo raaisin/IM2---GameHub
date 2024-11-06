632350cc457ad9124f0dc4b0c2ed41fe3f10bfd7
Hanz Bacus <bacushanzchester@gmail.com>
2024-11-06 15:05:14 +0800

    log in update

--- accounts/models.py
+++ accounts/models.py
@@ -56,7 +56,7 @@ class OrderItem(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return f'{self.product.name} in Order {self.order.id}'  # Changed from product_name to name
+        return f'{self.product.name} in Order {self.order.id}'  
 
 class PaymentMethod(models.Model):  # Fixed class name and made it a proper model
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
